dbe5091a76beffeef369fa9c43da1a31d49ca868
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-02-26 15:31:31 +0100

    Added file with route modules to speed up route creation.

---
+++ js/route-modules.js
@@ -0,0 +1,33 @@
+[
+    // Middle to middle 1
+    {x: 20, y: 15},
+    {x: 25, y: 13},
+    {x: 21, y: 10},
+    {x: 19, y: 10},
+    {x: 15, y: 12},
+    {x: 25, y: 18},
+    {x: 23, y: 15},
+    {x: 19, y: 18},
+    {x: 21, y: 18},
+    {x: 20, y: 15},
+
+    // Middle to middle 2
+
+    // Middle to right 1
+
+    // Right to right 1
+
+    // Right to right 2
+
+    // Right to middle 1
+
+    // Middle to left 1
+
+    // Left to left 1
+
+    // Left to left 2
+
+    // Left to outside left 1
+
+    // Outside left to outside left 1 
+];
